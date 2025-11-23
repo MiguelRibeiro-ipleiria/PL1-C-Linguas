6fa061039fc4f83cadc5994846e015ebbaba71d6
GuilhermeFerreiraa123 <fonsecafguilherme@gmail.com>
2025-11-23 16:50:53 +0000

    nada
    nada

--- linguasproject/backend/views/layouts/sidebar.php
+++ linguasproject/backend/views/layouts/sidebar.php
@@ -1,9 +1,15 @@
+<?php
+use yii\bootstrap5\Html;
+use yii\helpers\Url;
+
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+    <?= html::a('
+        <img src="'.$assetDir.'/img/AdminLTELogo.png" class="brand-image img-circle elevation-3" style="opacity:.8">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>',['/'], ['class' => 'brand-link', 'encode' => false]
+    )
+    ?>
 
     <!-- Sidebar -->
     <div class="sidebar">
