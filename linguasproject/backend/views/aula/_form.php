2cf4827c761b5817be287901d372cf50263f4833
MiguelRibeiro-ipleiria <2241595@my.ipleiria.pt>
2025-11-30 15:46:44 +0000

    Mudança no _form das aulas

--- linguasproject/backend/views/aula/_form.php
+++ linguasproject/backend/views/aula/_form.php
@@ -2,8 +2,14 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+<<<<<<< HEAD
 use yii\helpers\ArrayHelper;
 use common\models\Curso;
+=======
+use common\models\Curso;
+use yii\helpers\ArrayHelper;
+
+>>>>>>> 3994628d8b9aaf6718ebd04e43e6e5b4b35615d9
 /** @var yii\web\View $this */
 /** @var common\models\aula $model */
 /** @var yii\widgets\ActiveForm $form */
@@ -11,7 +17,12 @@ use common\models\Curso;
 
 <div class="aula-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin();
+
+        $arraycursos = ArrayHelper::map(curso::find()->all(), 'id', 'titulo_curso');
+    
+
+    ?>
 
     <div class="row">
         <div class="col-9">
@@ -34,6 +45,18 @@ use common\models\Curso;
         </div>
     </div>
 
+<<<<<<< HEAD
+=======
+    <?= $form->field($model, 'descricao_aula')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'numero_de_exercicios')->textInput() ?>
+
+    <?= $form->field($model, 'tempo_estimado')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'data_criacao')->textInput(['maxlength' => true]) ?>
+>>>>>>> 3994628d8b9aaf6718ebd04e43e6e5b4b35615d9
+
+    <?= $form->field($model, 'curso_id')->dropDownList($arraycursos) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Guardar Aula', ['class' => 'btn btn-success']) ?>
